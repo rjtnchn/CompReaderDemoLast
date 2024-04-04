c9abf8d92891ba20d9193f2b633a5b7df4197239
rjtnchn <24riexd@gmail.com>
2024-04-04 15:25:47 +0800

    minor fixes

--- end3.js
+++ end3.js
@@ -1,3 +1,3 @@
 const finalScore3 = document.getElementById("finalScore3");
 const mostRecentScore3 = localStorage.getItem("mostRecentScore3");
-finalScore3.innerText = "Score: " + mostRecentScore3 + "/" + "10";
+finalScore3.innerText = "Score: " + mostRecentScore3 + "/" + "5";
